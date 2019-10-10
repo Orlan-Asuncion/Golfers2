f078620c847841f2b0ff57173e2351447a100413
Orlan-Asuncion <48038688+Orlan-Asuncion@users.noreply.github.com>
2019-10-10 19:03:57 -0400

    Update README.md

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     golferRoutes     = require("./routes/golfers"),
     indexRoutes      = require("./routes/index");
-var url = process.env.DATABASEURL || "mongodb://localhost:27017/Golfers2";
-mongoose.connect(url) ;
-
+// var url = process.env.DATABASEURL || "mongodb://localhost/Golfers2";
+// mongoose.connect(url) ;
+mongoose.connect("mongodb://<username>:<password>@ds311538.mlab.com:11538/heroku_79xr3x52");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
---
+++ readme.md
@@ -0,0 +1,24 @@
+## Overview
+
+This app is structured following the RESTful Routes. The user will be able to Create, Post, Edit and Delete a post. Landing page animation main component of CSS animation is @keyframes. Passport is used for user authentication.
+
+## RESTful Routes Chart by Colt Steele
+
+
+## Technologies/Tools Used
+
+* Express
+* Passport
+* Mongoose
+* Express body-parser
+* EJS
+* Method-override
+* Bootstrap
+* Passport Local
+* Passport Local Mongoose
+* Connect Flash
+* Express Sessions
+
+
+
+
